916b6b28dfb3a6eecd349ad0d34575afb6092a69
waitingforthese <waitingforthesemoments@gmail.com>
2026-08-16 15:36:23 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -10,29 +9,32 @@ android {
 
     defaultConfig {
         applicationId = "com.mahaesuvidha.chandrapanchangalarm"
-        minSdk = 26
+        minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "1.0"
     }
 
-    buildFeatures { compose = true }
-compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
+    buildFeatures {
+        compose = true
+    }
 
-kotlinOptions {
-    jvmTarget = "17"
-}
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
 
 dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
     implementation("androidx.compose.ui:ui:1.7.8")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
+    implementation("androidx.compose.material:material:1.7.8")
     implementation("androidx.compose.material3:material3:1.3.1")
     implementation("com.google.android.gms:play-services-location:21.3.0")
 }
