9d3e8982a9641dc8c82e83aa352e16c1e842104a
waitingforthese <waitingforthesemoments@gmail.com>
2026-08-16 22:27:03 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mahaesuvidha.chandrapanchangalarm"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 4
+        versionName = "2.4"
     }
 
     buildFeatures { compose = true }
