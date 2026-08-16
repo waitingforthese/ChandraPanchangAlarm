a86dfe7edea1962996311bd14e3c5b7f0fbd0155
waitingforthese <waitingforthesemoments@gmail.com>
2026-08-16 17:20:04 +0530

    Update LiveMoonCalculator.kt

--- app/src/main/kotlin/com/mahaesuvidha/chandrapanchangalarm/model/LiveMoonCalculator.kt
+++ app/src/main/kotlin/com/mahaesuvidha/chandrapanchangalarm/model/LiveMoonCalculator.kt
@@ -1,1 +1,28 @@
 
+package com.mahaesuvidha.chandrapanchangalarm.model
+
+import java.time.LocalDateTime
+
+object LiveMoonCalculator {
+
+    fun getCurrentMoonState(): MoonState {
+
+        /*
+         * सध्या हा LIVE calculation integration साठी तयार base आहे.
+         * पुढच्या step मध्ये actual astronomical calculation जोडणार आहोत.
+         */
+
+        val now = LocalDateTime.now()
+
+        return MoonState(
+            location = "दौंड, महाराष्ट्र",
+            rashi = Rashi.KARKA,
+            nakshatra = Nakshatra.PUSHYA,
+            pada = 2,
+            nextChange = "Live calculation सुरू आहे",
+            nextChangeTime =
+                "${now.dayOfMonth}-${now.monthValue}-${now.year} " +
+                "${now.hour}:${String.format("%02d", now.minute)}"
+        )
+    }
+}
