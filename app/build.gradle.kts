e3c36f0a5f644a0cace99ac4f11211942528c0e6
waitingforthese <waitingforthesemoments@gmail.com>
2026-08-16 15:32:36 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,15 @@ android {
     }
 
     buildFeatures { compose = true }
+compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
+
+kotlinOptions {
+    jvmTarget = "17"
+}
+
 
 dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
