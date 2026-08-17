1c76608d7d6cccdee29cc698a07d345e3655b0a9
waitingforthese <waitingforthesemoments@gmail.com>
2026-08-18 00:36:17 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,5 +44,5 @@ dependencies {
     implementation("androidx.compose.ui:ui:1.7.8")
 
     implementation("androidx.compose.material3:material3:1.3.1")
-   implementation("com.github.cosinekitty:astronomy:2.1.19")
+   implementation("com.google.android.gms:play-services-location:21.3.0")
 }
