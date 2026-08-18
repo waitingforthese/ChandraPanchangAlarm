1aea0aab5c602f8dbd9ee313ee50e5a66f7fff7a
waitingforthese <waitingforthesemoments@gmail.com>
2026-08-18 23:25:38 +0530

    Update AlarmReceiver.kt

--- app/src/main/kotlin/com/mahaesuvidha/chandrapanchangalarm/alarm/AlarmReceiver.kt
+++ app/src/main/kotlin/com/mahaesuvidha/chandrapanchangalarm/alarm/AlarmReceiver.kt
@@ -9,7 +9,6 @@ import android.media.AudioAttributes
 import android.net.Uri
 import android.os.Build
 import androidx.core.app.NotificationCompat
-import com.mahaesuvidha.chandrapanchangalarm.R
 
 class AlarmReceiver : BroadcastReceiver() {
 
