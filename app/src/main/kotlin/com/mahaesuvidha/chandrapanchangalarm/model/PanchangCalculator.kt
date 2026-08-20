b48b11f4d679271d7fa16528475ae7f881cd4097
waitingforthese <waitingforthesemoments@gmail.com>
2026-08-20 23:49:19 +0530

    Update PanchangCalculator.kt

--- app/src/main/kotlin/com/mahaesuvidha/chandrapanchangalarm/model/PanchangCalculator.kt
+++ app/src/main/kotlin/com/mahaesuvidha/chandrapanchangalarm/model/PanchangCalculator.kt
@@ -396,6 +396,29 @@ val lagnaData =
                 toMillis(
                     nextMasa.second
                 )
+            // PRAHAR
+
+prahar = praharData.current,
+
+nextPrahar = praharData.next,
+
+nextPraharTime =
+    formatDateTime(praharData.nextTime),
+
+nextPraharMillis =
+    toMillis(praharData.nextTime),
+
+// LAGNA
+
+lagna = lagnaData.current,
+
+nextLagna = lagnaData.next,
+
+nextLagnaTime =
+    formatDateTime(lagnaData.nextTime),
+
+nextLagnaMillis =
+    toMillis(lagnaData.nextTime)
         )
     }
 
