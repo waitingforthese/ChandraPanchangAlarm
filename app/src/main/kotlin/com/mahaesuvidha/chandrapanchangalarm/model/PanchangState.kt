42b71b32ede38564f37697e8989246706c131447
waitingforthese <waitingforthesemoments@gmail.com>
2026-08-20 23:42:58 +0530

    Update PanchangState.kt

--- app/src/main/kotlin/com/mahaesuvidha/chandrapanchangalarm/model/PanchangState.kt
+++ app/src/main/kotlin/com/mahaesuvidha/chandrapanchangalarm/model/PanchangState.kt
@@ -1,46 +1,4 @@
-package com.mahaesuvidha.chandrapanchangalarm.model
-
-data class PanchangState(
-
-    val date: String,
-    val weekday: String,
-
-    // TITHI
-    val tithi: String,
-    val tithiStartTime: String = "—",
-    val nextTithi: String,
-    val nextTithiTime: String,
-    val nextTithiMillis: Long,
-
-    // YOGA
-    val yoga: String,
-    val yogaStartTime: String = "—",
-    val nextYoga: String,
-    val nextYogaTime: String,
-    val nextYogaMillis: Long,
-
-    // KARANA
-    val karana: String,
-    val karanaStartTime: String = "—",
-    val nextKarana: String,
-    val nextKaranaTime: String,
-    val nextKaranaMillis: Long,
-
-    // PAKSHA
-    val paksha: String,
-    val pakshaStartTime: String = "—",
-    val nextPaksha: String,
-    val nextPakshaTime: String,
-    val nextPakshaMillis: Long,
-
-    // MAS
-    val masa: String = "—",
-    val nextMasa: String = "—",
-    val masaStartTime: String = "—",
-    val nextMasaTime: String = "—",
-    val nextMasaMillis: Long = 0L,
-
-    // PRAHAR
+// PRAHAR
 
 prahar =
     praharData.current,
@@ -76,4 +34,3 @@ nextLagnaMillis =
     toMillis(
         lagnaData.nextTime
     )
-)
